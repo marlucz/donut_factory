afbcbcc09de24aeb5d4a2d8673424d8230fd2267
marlucz <marcin_luczak@hotmail.com>
2022-02-02 23:27:44 +0100

    Add dough model and animation

--- src/Experience/World/Donut.js
+++ src/Experience/World/Donut.js
@@ -25,38 +25,49 @@ export default class Donut {
 
   setModel() {
     this.model = {};
-    console.log(this.resources.items.donutModel);
-    this.model.group = this.resources.items.donutModel.scene;
-    this.scene.add(this.model.group);
+     this.resources.items.donutModel.scene.traverse(child => {
+       if(child.name === 'Donut') this.model.donut = child;
+       if(child.name === 'Dough') this.model.dough = child;
+     })
+     console.log(this.model)
+    this.scene.add(this.model.donut, this.model.dough);
   }
 
   setAnimation() {
     this.animation = {};
     this.animation.isActive = false;
+    this.animation.mixer = {}
+    this.animation.mixer.donut = new THREE.AnimationMixer(this.model.donut);
+    this.animation.mixer.dough = new THREE.AnimationMixer(this.model.dough);
 
-    this.animation.mixer = new THREE.AnimationMixer(this.model.group);
-
-    this.animation.mixer.addEventListener("finished", () => {
+    this.animation.mixer.donut.addEventListener("finished", () => {
       this.animation.isActive = false;
-      this.animation.mixer.time = 0;
-      console.log(this.animation.mixer)
+      this.animation.mixer.donut.time = 0;
+    });
+    this.animation.mixer.dough.addEventListener("finished", () => {
+      this.animation.isActive = false;
+      this.animation.mixer.dough.time = 0;
     });
 
     this.animation.actions = {};
-    this.animation.actions.toBake = this.animation.mixer.clipAction(
+
+    this.animation.actions.toBake = this.animation.mixer.donut.clipAction(
       this.resources.items.donutModel.animations.find(anim => anim.name === "donut-to-bake")
     );
-    this.animation.actions.bake = this.animation.mixer.clipAction(
+    this.animation.actions.bake = this.animation.mixer.donut.clipAction(
       this.resources.items.donutModel.animations.find(anim => anim.name === "donut-bake")
     );
-    this.animation.actions.toIcing = this.animation.mixer.clipAction(
+    this.animation.actions.toIcing = this.animation.mixer.donut.clipAction(
        this.resources.items.donutModel.animations.find(anim => anim.name === "donut-to-icing")
       );
-    this.animation.actions.toSprinkles = this.animation.mixer.clipAction(
+    this.animation.actions.toSprinkles = this.animation.mixer.donut.clipAction(
        this.resources.items.donutModel.animations.find(anim => anim.name === "donut-to-sprinkles")
       );
-    this.animation.actions.leave = this.animation.mixer.clipAction(
+    this.animation.actions.leave = this.animation.mixer.donut.clipAction(
        this.resources.items.donutModel.animations.find(anim => anim.name === "donut-leave")
+      );
+    this.animation.actions.toForm = this.animation.mixer.dough.clipAction(
+       this.resources.items.donutModel.animations.find(anim => anim.name === "dough-to-form")
       );
 
     this.animation.actions.current = this.animation.toBake;
@@ -64,15 +75,14 @@ export default class Donut {
     this.animation.play = (name) => {
 
       const action = this.animation.actions[name];
-
       action.reset();
       action.setLoop(THREE.LoopOnce);
       action.clampWhenFinished = true;
       action.play();
       this.animation.isActive = true;
-      console.log(this.animation.mixer)
 
       this.animation.actions.current = action;
+      console.log(this.animation.actions.current)
     };
 
     if (this.debug) {
@@ -106,6 +116,12 @@ export default class Donut {
           label: "play leave",
         })
         .on("click", () => this.startAnimation("leave"));
+      this.debugFolder
+        .addButton({
+          title: "toForm",
+          label: "play toForm",
+        })
+        .on("click", () => this.startAnimation("toForm"));
     }
   }
 
@@ -115,7 +131,12 @@ export default class Donut {
 
   update() {
     if (this.animation.isActive) {
-      this.animation.mixer.update(0.01);
+      this.animation.mixer.donut.update(0.005);
+      this.animation.mixer.dough.update(0.005);
+
+      const {x,z} = this.model.donut.position
+
+      // console.log('x: ' + x, 'z ' + z)
     }
   }
 }
